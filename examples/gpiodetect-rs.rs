8e2c8502ad37eacc6fbe3f1ae5754fdfb92ef61c
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2019-09-18 21:00:37 +0200

    Small clean-up of gpiodetect

--- examples/gpiodetect-rs.rs
+++ examples/gpiodetect-rs.rs
@@ -1,17 +1,15 @@
-use std::path::Path;
 use std::fs;
-use std::env;
-
 use libgpio::libgpio::GpioChip;
 
 fn main() {
     let dev_files = fs::read_dir("/dev/").unwrap();
 
-    dev_files.
-        filter_map(Result::ok)
-        .filter(|f| f.path().to_str().unwrap().starts_with("/dev/gpiochip"))
-        .for_each(|f| println!("{} [{}] ({} lines)"
-            ,GpioChip::new(&f.path()).unwrap().name()
-            ,GpioChip::new(&f.path()).unwrap().label()
-            ,GpioChip::new(&f.path()).unwrap().num_lines()));
+    let gpiochips : Vec<_> = dev_files.
+            filter_map(Result::ok)
+            .filter(|f| f.path().to_str().unwrap().starts_with("/dev/gpiochip"))
+            .map(|f| GpioChip::new(&f.path()).unwrap())
+            .collect();
+    
+    gpiochips.iter().rev() //Do it in reverse order because the numbers of the GPIO chips go from high to low
+        .for_each(|f| println!("{} [{}] ({} lines)",f.name(),f.label(),f.num_lines()));
 }
